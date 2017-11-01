8210a83b7c73459f0f618581b14c751b06595401
GUUSTA <gustavo031199@gmail.com>
2017-10-31 22:34:30 -0200

    aula 31/10

---
+++ Fila.c
@@ -0,0 +1,81 @@
+#include <stdio.h>
+#include <stdlib.h>
+
+typedef struct{
+  int vetor[10];
+  int topo;
+  int inicio;
+} Fila;
+
+void inicializaFila();
+void inserir();
+int cheio();
+int vazio();
+void imprimeFila();
+
+int main() {
+
+    Fila minhafila;
+    int temp;
+
+    inicializaFila(&minhafila);
+
+    inserir(&minhafila, 19);
+    inserir(&minhafila, 28);
+    inserir(&minhafila, 30);
+    inserir(&minhafila, 12);
+    imprimeFila(&minhafila);
+    temp=remover(&minhafila);
+    temp=remover(&minhafila);
+    inserir(&minhafila, 51);
+    inserir(&minhafila, 48);
+    imprimeFila(&minhafila);
+
+    return 0;
+}
+
+void inicializaFila(Fila *p){
+  p->topo=0;
+  p->inicio=0;
+}
+
+int cheio(Fila *p) {
+    if(p->topo >= 10)
+        return 1;
+    return 0;
+}
+
+int vazio (Fila *p) {
+    if(p->topo == 0)
+        return 1;
+    return 0;
+}
+
+void inserir(Fila *p, int valor) {
+    if(cheio(p) == 1) {
+        printf("ERRO, CHEIO");
+        return;
+    }
+    p->vetor[p->topo] = valor;
+    p->topo++;
+}
+
+void imprimeFila(Fila *p){
+  int i;
+  for(i=0; i<p->topo; i++){
+    printf("%d ", p->vetor[i]);
+  }
+  printf("\n");
+}
+
+int remover(Fila *p){
+    if(vazio(p) == 1) {
+        printf("ERRO, VAZIO");
+        return;
+    }
+
+    p->inicio++;
+    return p->vetor[p->topo];
+
+}
+
