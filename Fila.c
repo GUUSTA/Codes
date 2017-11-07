3f1d06804b54bb1bf4e51449feb97ddeadf5521f
GUUSTA <gustavo031199@gmail.com>
2017-11-07 21:53:28 -0200

    Fila_Vetor

--- Fila.c
+++ Fila.c
@@ -1,59 +1,76 @@
 #include <stdio.h>
 #include <stdlib.h>
+#define tam 10
 
 typedef struct{
-  int vetor[10];
-  int topo;
-  int inicio;
+    int tamanho;
+    int vetor[tam];
+    int topo;
+    int inicio;
 } Fila;
 
-//void inicializaFila();
-//int cheio();
-//int vazio();
-//void inserir();
-//int remover();
-//void imprimeFila();
-
 void inicializaFila(Fila *p){
-  p->topo=0;
-  p->inicio=0;
+    p->tamanho = 0;
+    p->topo = 0;
+    p->inicio = 0;
 }
 
 int cheio(Fila *p) {
-    if(p->topo >= 10)
+    if(p->topo > 9)
         return 1;
     return 0;
 }
 
 int vazio (Fila *p) {
-    if(p->topo == 0)
+    if(p->tamanho == 0)
         return 1;
     return 0;
 }
 
 void inserir(Fila *p, int valor) {
-    if(cheio(p) == 1) {
-        printf("ERRO, CHEIO");
-        return;
+
+    if(cheio(p) == 1 && p->tamanho != tam) {
+        p->topo = 0;
+        p->vetor[p->topo] = valor;
+        p->tamanho++;
+        p->topo++;
     }
-    p->vetor[p->topo] = valor;
-    p->topo++;
+    else if (p->tamanho == tam){
+        printf("ERRO, CHEIO\n");
+        return;
+    } else
+    {
+        p->vetor[p->topo] = valor;
+        p->topo++;
+        p->tamanho++;
+    }
 }
 
 int remover(Fila *p){
     if(vazio(p) == 1) {
-        printf("ERRO, VAZIO");
+        printf("ERRO, VAZIO\n");
         return;
     }
-
+    p->tamanho--;
     p->inicio++;
     return p->vetor[p->inicio];
 }
 
 void imprimeFila(Fila *p){
   int i;
-  for(i=p->inicio; i<p->topo; i++){
-    printf("%d ", p->vetor[i]);
+  int temp=0;
+  int k = p->inicio;
+  for(i=0; i<p->tamanho; i++){
+
+    printf("%d ", p->vetor[k]);
+    k++;
+    if(k > 9)
+    {
+        temp = p->inicio;
+        p->inicio = 0;
+        p->vetor[p->topo];
+        k = 0;
+    }
   }
   printf("\n");
 }
@@ -73,13 +90,15 @@ int main() {
     temp=remover(&minhafila);
     temp=remover(&minhafila);
     inserir(&minhafila, 51);
-    inserir(&minhafila, 48);
+    inserir(&minhafila, 51);
+    inserir(&minhafila, 51);
+    inserir(&minhafila, 51);
+    inserir(&minhafila, 51);
+    inserir(&minhafila, 21);
+    inserir(&minhafila, 31);
+    inserir(&minhafila, 41);
+    //inserir(&minhafila, 41);
     imprimeFila(&minhafila);
 
     return 0;
 }
-
-
-
-
-
