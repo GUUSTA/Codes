34c3863facd46f62572c93b2dc90e6f86b95e0d7
Gustavo Garcia Leite <gustavo031199@gmail.com>
2019-04-12 20:58:01 -0300

    update layout

--- Quinto-Semestre/PI5/PI5/ViewController.swift
+++ Quinto-Semestre/PI5/PI5/ViewController.swift
@@ -15,7 +15,8 @@ enum Taxa {
 
 class ViewController: UIViewController {
     
-    
+    @IBOutlet weak var imgNuvemGrande: UIImageView!
+    @IBOutlet weak var imgNuvemPequena: UIImageView!
     @IBOutlet weak var lblTaxa: UILabel!
     @IBOutlet weak var txtCompA: UITextField!
     @IBOutlet weak var txtQtdDias: UITextField!
@@ -30,6 +31,11 @@ class ViewController: UIViewController {
     @IBOutlet weak var lblValorRetirado: UILabel!
     @IBOutlet weak var lblValorRecebido: UILabel!
     
+    
+    var imgsNuvens: [UIImage] = [UIImage(named: "nuvem1")!, UIImage(named: "nuvem2")!, UIImage(named: "nuvem3")!, UIImage(named: "nuvem4")!, UIImage(named: "nuvem5")!, UIImage(named: "nuvem6")!, UIImage(named: "nuvem7")!, UIImage(named: "nuvem8")!, UIImage(named: "nuvem9")!, UIImage(named: "nuvem10")!, UIImage(named: "nuvem9")!, UIImage(named: "nuvem8")!, UIImage(named: "nuvem7")!, UIImage(named: "nuvem6")!, UIImage(named: "nuvem5")!, UIImage(named: "nuvem4")!, UIImage(named: "nuvem3")!, UIImage(named: "nuvem2")!, UIImage(named: "nuvem1")!]
+    
+    var imgsNuvensPequena: [UIImage] = [UIImage(named: "nuvem11")!, UIImage(named: "nuvem12")!, UIImage(named: "nuvem13")!, UIImage(named: "nuvem14")!, UIImage(named: "nuvem15")!, UIImage(named: "nuvem16")!, UIImage(named: "nuvem17")!, UIImage(named: "nuvem18")!, UIImage(named: "nuvem19")!, UIImage(named: "nuvem20")!, UIImage(named: "nuvem19")!, UIImage(named: "nuvem18")!, UIImage(named: "nuvem17")!, UIImage(named: "nuvem16")!, UIImage(named: "nuvem15")!, UIImage(named: "nuvem14")!, UIImage(named: "nuvem13")!, UIImage(named: "nuvem12")!, UIImage(named: "nuvem11")!]
+    
     var timer: Timer = Timer()
     var timerInterval: TimeInterval = 1.0
     var minutos = 0
@@ -43,23 +49,39 @@ class ViewController: UIViewController {
     var qtdDias = 10
     var tipoTempo = 1440
     var tipoTaxa = Taxa.minutos
+    var isSimulating = false
+    
     
     override func viewDidLoad() {
         super.viewDidLoad()
+        
+        imgNuvemGrande.animationImages = imgsNuvens
+        imgNuvemGrande.animationDuration = 2.0
+        imgNuvemGrande.animationRepeatCount = 1000
+        imgNuvemGrande.startAnimating()
+        
+        imgNuvemPequena.animationImages = imgsNuvensPequena
+        imgNuvemPequena.animationDuration = 2.0
+        imgNuvemPequena.animationRepeatCount = 1000
+        imgNuvemPequena.startAnimating()
         
         
     }
     
     @IBAction func simular(_ sender: UIButton) {
+        
+        guard !isSimulating else { return }
+        guard txtCompA.text != "", txtQtdDias.text != "", txtTaxa.text != "" else { return }
+        
         setup()
         qtdDias = Int((txtQtdDias.text! as NSString).intValue)
         calcula(compartimentoA: (txtCompA.text! as NSString).doubleValue, compartimentoB: 0.0, tipoTaxa: tipoTaxa, taxa: (txtTaxa.text! as NSString).doubleValue)
         
         startTimer()
+        isSimulating = true
     }
     @IBAction func cancelar(_ sender: UIButton) {
-        timer.invalidate()
-        minutos = 0
+        stop()
     }
     
     func calcula(compartimentoA: Double, compartimentoB: Double, tipoTaxa: Taxa, taxa: Double) {
@@ -104,10 +126,10 @@ class ViewController: UIViewController {
     
     func setup() {
         
-        self.cBView.frame.size.height = 230.0
-        self.cBView.frame.origin.y = 251
+        self.cBView.frame.size.height = 168.0
+        self.cBView.frame.origin.y = 294
         self.cAView.frame.size.height = 1.0
-        self.cAView.frame.origin.y = 251
+        self.cAView.frame.origin.y = 294
         valoresCA.removeAll()
         valoresCB.removeAll()
         valoresCC.removeAll()
@@ -126,9 +148,9 @@ class ViewController: UIViewController {
             case 0:
                 timerInterval = 1
             case 1:
-                timerInterval = 0.0050
+                timerInterval = 0.05
             case 2:
-                timerInterval = 0.0025
+                timerInterval = 0.001
             default:
                 break
         }
@@ -145,7 +167,6 @@ class ViewController: UIViewController {
         default:
             break
         }
-        
     }
     
     @objc func update(){
@@ -156,14 +177,19 @@ class ViewController: UIViewController {
             lblValorRecebido.text = "+\(valoresCC[minutos])/\(novoTipoTaxa)"
             lblValorRetirado.text = "-\(valoresCC[minutos])/\(novoTipoTaxa)"
             self.cBView.frame.size.height -= CGFloat(quant[minutos])
-            self.cBView.frame.origin.y = 251
+            self.cBView.frame.origin.y = 294
             self.cAView.frame.size.height += CGFloat(quant[minutos])
-            self.cAView.frame.origin.y = 251
+            self.cAView.frame.origin.y = 294
             minutos += 1
         }
         else if minutos > tipoTempo * qtdDias {
-            timer.invalidate()
-            minutos = 0
+            stop()
         }
+    }
+    
+    func stop() {
+        isSimulating = false
+        timer.invalidate()
+        minutos = 0
     }
 }
