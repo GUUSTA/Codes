fc2c1295d08943f85f850f01802a65b3360947cd
Gustavo Garcia Leite <gustavo031199@gmail.com>
2019-05-20 07:29:32 -0300

    updates

--- Quinto-Semestre/PI5/PI5/ViewController.swift
+++ Quinto-Semestre/PI5/PI5/ViewController.swift
@@ -8,49 +8,27 @@
 import Foundation
 import UIKit
 
-//enum Taxa {
-//    case minutos
-//    case horas
-//}
-
 class ViewController: UIViewController {
 
-    @IBOutlet weak var lblTaxa: UILabel!
-    @IBOutlet weak var txtCompA: UITextField!
-    @IBOutlet weak var txtQtdDias: UITextField!
-    @IBOutlet weak var txtTaxa: UITextField!
     @IBOutlet weak var btnSimular: UIButton!
     @IBOutlet weak var lblTempo: UILabel!
     @IBOutlet weak var cAView: UIView!
     @IBOutlet weak var aView: UIView!
     @IBOutlet weak var cAImageView: UIImageView!
-    @IBOutlet weak var cBView: UIView!
     @IBOutlet weak var lblCA: UILabel!
-    @IBOutlet weak var lblCB: UILabel!
-    @IBOutlet weak var lblValorRetirado: UILabel!
-    @IBOutlet weak var lblValorRecebido: UILabel!
     @IBOutlet weak var agua: UIImageView!
     @IBOutlet weak var imgNuvemPAzul: UIImageView!
     @IBOutlet weak var imgNuvemPCinza: UIImageView!
     @IBOutlet weak var imgNuvemGCinza: UIImageView!
     @IBOutlet weak var imgNuvemGAzul: UIImageView!
-    @IBOutlet weak var imgAguaCano: UIView!
-    
     @IBOutlet weak var imgCeuDia: UIImageView!
-    
     @IBOutlet weak var imgCeuNoite: UIImageView!
-    
     @IBOutlet weak var imgSol: UIImageView!
-    
     @IBOutlet weak var imgLua: UIImageView!
-    
-    
     @IBOutlet weak var lblCompartimentoA: UILabel!
     @IBOutlet weak var btnAdicionar: UIButton!
     @IBOutlet weak var btnRemover: UIButton!
-    
     @IBOutlet weak var txtQtdCompA: UITextField!
-    
     @IBOutlet weak var txtTaxaA: UITextField!
     @IBOutlet weak var lblDias: UILabel!
     
@@ -58,14 +36,13 @@ class ViewController: UIViewController {
     var timerInterval: TimeInterval = 1.0
     var segundos = 0
     var isAnimating = false
-    
     var valoresCA = [Double]()
     var valoresCB = [Double]()
     var valoresCC = [Double]()
     var quant = [Double]()
     
     let alfabeto: [String] = ["A", "B", "C", "D", "E", "F", "G", "H", "I"]
-
+    var matrizValores: [[Double]] = [[Double]]()
     var compartimentos: [Double] = [1000, 0, 0]
     var diferencas: [Double] = []
     var copiaCompartimentos: [Double] = []
@@ -85,13 +62,6 @@ class ViewController: UIViewController {
     var isSimulating = false
     var k = 20
     
-    
-    
-    var duracao = 0
-    var diferenca = 0.0
-//    var velocidade = 1
-    
-    let corAzul = UIColor(red: 165, green: 241, blue: 255, alpha: 1)
     
     override func viewDidLoad() {
         super.viewDidLoad()
@@ -260,6 +230,10 @@ class ViewController: UIViewController {
         for txt in arrayTxtQtd {
             compartimentos.append((txt.text! as NSString).doubleValue)
         }
+        
+        for j in 0...arrayLabelsValores.count - 1 {
+            arrayLabelsValores[j].text = "\(compartimentos[j])"
+        }
     }
     
     func fazFormula(arrayComp: [Double], qtd: Double, arrayK: [Double]) -> [Double] {
@@ -298,10 +272,23 @@ class ViewController: UIViewController {
             let k4 = fazFormula(arrayComp: copyArray, qtd: h, arrayK: k3)
             copyArray = determinaNovoValor(arrayComp: copyArray, h: h, k1: k1, k2: k2, k3: k3, k4: k4)
             j += h
-            //print("\(j) ", copyArray)
+            if round(j * 1)/1 == round(j * 100000000000)/100000000000 {
+                var possui = false
+                for array in matrizValores {
+                    if copyArray.first == array.first {
+                        possui = true
+                    }
+                }
+                if !possui {
+                    matrizValores.append(copyArray)
+                    print("append: ", copyArray)
+                }
+                
+            }
             
             retornaSoma(array: copyArray)
         }
+        print("Final: ", copyArray)
         compartimentos = copyArray
     }
     
@@ -318,7 +305,6 @@ class ViewController: UIViewController {
         for item in array {
             soma += item
         }
-        //print(soma)
     }
     
     func determinaDiferenca() {
@@ -335,13 +321,6 @@ class ViewController: UIViewController {
     
     func animacoes() {
         
-        UIView.animate(withDuration: 1, delay: 0.0, options: [.repeat], animations: {
-            
-            
-            self.imgAguaCano.frame.size.width += 117
-            
-        }, completion: nil)
-        
         UIView.animate(withDuration: 2, delay: 0.0, options: [.repeat, .autoreverse], animations: {
             
             
@@ -357,18 +336,6 @@ class ViewController: UIViewController {
             self.imgNuvemGCinza.frame.origin.x += CGFloat(self.k)
         
         }, completion:  nil)
-//        }, completion:{(finished: Bool) in
-//
-//            if self.isSimulating {
-//                self.k = 100
-//            } else {
-//                self.k = 20
-//            }
-//
-//        })
-        
-        
-        
     }
     
     func verificaTxtField() -> Bool {
@@ -394,15 +361,13 @@ class ViewController: UIViewController {
         
         guard !isSimulating else { return }
         guard verificaTxtField() else { return }
-        //guard txtCompA.text != "", txtQtdDias.text != "", txtTaxa.text != "" else { return }
         setup()
         formataArrayTaxas()
         calcula()
-        atribuirValores()
+        //atribuirValores()
         determinaDiferenca()
         anima()
         startTimer()
-        //animacaCeu()
         isSimulating = true
     }
     
@@ -444,6 +409,12 @@ class ViewController: UIViewController {
                         self.segundos += 1
                     }
                 })
+                
+                for j in 0...matrizValores[segundos].count - 1 {
+                    arrayLabelsValores[j].text = "\(round(matrizValores[segundos][j] * 100000)/100000)"
+                }
+                
+                lblTempo.text = "Dia \(segundos + 1)"
             }
         } else {
             timer.invalidate()
@@ -468,24 +439,6 @@ class ViewController: UIViewController {
         }
     }
     
-//    func animaCompartimentos() {
-//
-//        UIView.animate(withDuration: 3, animations: {
-//
-//            self.cAView.frame.size.height = self.cAView.frame.height + CGFloat(self.diferenca)
-//            self.cBView.frame.size.height = self.cBView.frame.height - CGFloat(self.diferenca)
-//
-//        }, completion: { (finish: Bool) in
-//
-//            self.isSimulating = false
-//            self.imgAguaCano.isHidden = true
-//
-//        } )
-//
-//        lblCA.text = "\(cA)"
-//        lblCB.text = "\(cB)"
-//    }
-    
     func setup() {
         for i in 0...arrayCViews.count - 1 {
             if i == 0 {
@@ -498,175 +451,10 @@ class ViewController: UIViewController {
                 arrayViews[i].frame = CGRect(x: arrayCViews[i].frame.origin.x, y: arrayCViews.first!.frame.origin.y, width: 144, height: 96)
             }
         }
-        
+        lblTempo.isHidden = false
+        lblTempo.text = "Dia \(1)"
+        self.matrizValores = []
         self.imgCeuNoite.alpha = 1
         self.imgLua.alpha = 1
     }
-    
-//    @objc func update(){
-//
-//
-//
-//
-//        switch tipoTaxa {
-//        case .minutos:
-//            trocaDia()
-//            if minutos <= tipoTempo * qtdDias {
-//                lblTempo.text = "\(diasM) dias \(horasM) horas \(minutosM) minutos"
-//                lblCA.text = "\(valoresCA[minutos])"
-//                lblCB.text = "\(valoresCB[minutos])"
-//                lblValorRecebido.text = "+\(valoresCC[minutos])/\(novoTipoTaxa)"
-//                lblValorRetirado.text = "-\(valoresCC[minutos])/\(novoTipoTaxa)"
-//                self.cBView.frame.size.height -= CGFloat(quant[minutos])
-//                self.cBView.frame.origin.y = 294
-//                self.cAView.frame.size.height += CGFloat(quant[minutos])
-//                self.cAView.frame.origin.y = 294
-//                minutosM += 1
-//                minutos += 1
-//            } else if minutos > tipoTempo * qtdDias  {
-//                stop()
-//            }
-//        case .horas:
-//            trocaDia()
-//            if horas <= tipoTempo * qtdDias {
-//                lblTempo.text = "\(diasH) dias \(horasH) horas"
-//                lblCA.text = "\(valoresCA[horas])"
-//                lblCB.text = "\(valoresCB[horas])"
-//                lblValorRecebido.text = "+\(valoresCC[horas])/\(novoTipoTaxa)"
-//                lblValorRetirado.text = "-\(valoresCC[horas])/\(novoTipoTaxa)"
-//                self.cBView.frame.size.height -= CGFloat(quant[horas])
-//                self.cBView.frame.origin.y = 294
-//                self.cAView.frame.size.height += CGFloat(quant[horas])
-//                self.cAView.frame.origin.y = 294
-//                horasH += 1
-//                horas += 1
-//            } else if horas > tipoTempo * qtdDias  {
-//                stop()
-//            }
-//        }
-//    }
-    
-//    func stop() {
-//        isSimulating = false
-//        imgAguaCano.isHidden = true
-//        timer.invalidate()
-//        minutos = 0
-//        horas = 0
-//        minutosM = 0
-//        horasM = 0
-//        diasM = 0
-//        horasH = 0
-//        diasH = 0
-//    }
-    
-//    func trocaDia() {
-//        switch tipoTaxa {
-//        case .minutos:
-//            if minutosM >= 60 {
-//                minutosM = 0
-//                horasM += 1
-//            }
-//            if horasM > 23 {
-//                horasM = 0
-//                diasM += 1
-//                isSimulating = false
-//            }
-//            if horas == 13 {
-//                isAnimating = false
-//            }
-//
-//            //fazAnimacao(horario: horasM)
-//        case .horas:
-//            if horasH > 23 {
-//                horasH = 0
-//                diasH += 1
-//                isAnimating = false
-//            }
-//            if horasH == 13 {
-//                isAnimating = false
-//            }
-//            fazAnimacao(horario: horasH)
-//        }
-//    }
-    
-//    func fazAnimacao(horario: Int) {
-//        guard isSimulating else { return }
-//        guard !isAnimating else { return }
-//
-//
-//        //1.35
-//        //0.43
-//        if horario == 2 {
-//            isAnimating = true
-//
-//            //10
-//            //TimeInterval(duracao/2)
-//            // .curveEaseIn
-//            UIView.animate(withDuration: 10, delay: 0.0, options: [.curveEaseIn], animations: {
-//                self.imgCeuNoite.alpha = 0
-//                self.imgLua.alpha = 0
-//            }, completion:{(finished: Bool) in
-//                if finished {
-//                    self.isAnimating = false
-//                }
-//            })
-//        } else if horario == 13 {
-//            isAnimating = true
-//            //11
-//            //TimeInterval(duracao/2)
-//            UIView.animate(withDuration: 11, delay: 0.0, options: [.curveEaseIn], animations: {
-//                self.imgCeuNoite.alpha = 1
-//                self.imgLua.alpha = 1
-//            }, completion:{(finished: Bool) in
-//                if finished {
-//                   self.isAnimating = false
-//                }
-//            })
-//        }
-//
-//    }
-    
-//    func fazDuracao() {
-//        switch tipoTaxa {
-//        case .minutos:
-//            switch velocidade {
-//            case 1:
-//                duracao = 1440/1
-//            case 2:
-//                duracao = 1440/50
-//                print()
-//            case 3:
-//                duracao = 1440/100
-//            default:
-//                break
-//            }
-//        case .horas:
-//            switch velocidade {
-//            case 1:
-//                duracao = 24
-//            case 2:
-//                duracao = 24/50
-//            case 3:
-//                duracao = 24/100
-//            default:
-//                break
-//            }
-//        }
-//    }
 }
-
-
-/*
- Minuto:
-    - A cada 60 minutos, vira 1 hora
-    - A cada 12 horas set valor se é dia ou não
-    - A cada 24 horas vira 1 dia
-    - Caso for dia, faz animacao para ir para noite
-    - Caso for noite, faz animacao para ir de dia
- Horas:
-    - A cada 12 horas set valor se é dia ou não
-    - A cada 24 horas vira 1 dia
-     - Caso for dia, faz animacao para ir para noite
-     - Caso for noite, faz animacao para ir de dia
- */
-
